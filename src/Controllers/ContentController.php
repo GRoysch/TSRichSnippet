8a242fd85eee064f4ec072a5af9080a9f02e970f
Moritz Schwarz <groysch@yahoo.de>
2018-11-08 15:34:53 +0100

    Test commit

--- src/Controllers/ContentController.php
+++ src/Controllers/ContentController.php
@@ -4,11 +4,13 @@ namespace TSRichSnippet\Controllers;
 
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\Events\Dispatcher;
 
 class ContentController extends Controller
 {
-  public function sayHello(Twig $twig):string
+  public function sayHello(Twig $twig, Dispatcher $dispatcher):string
   {
+    print_r($dispatcher);
     return $twig->render('TSRichSnippet::content.hello');
   }
 }
